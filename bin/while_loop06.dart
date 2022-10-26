7f70ac4446a82b8806e512e4fe18f63e8eae101e
Shohrux2004 <shohruxergashev67@gmail.com>
2022-10-26 13:37:13 +0500

    3280

--- bin/while_loop06.dart
+++ bin/while_loop06.dart
@@ -2,4 +2,27 @@
   Create function func with "a " argument
   Return the larger of the odd and even numbers up to 'a', whichever is greater
 */
-void main() {}
+int func(int a) {
+  int c1, c2, b;
+  b = 0;
+  c1 = 0;
+  c2 = 0;
+  while (b <= a) {
+    if (b % 2 == 0) {
+      c1 += b;
+    }
+
+    if (b % 2 == 1) {
+      c2 += b;
+    }
+    b += 1;
+  }
+  if (c1 > c2) {
+    return c1;
+  }
+  return c2;
+}
+
+void main() {
+  print(func(5));
+}
