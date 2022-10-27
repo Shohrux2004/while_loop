5e14f27b1a70a95cc0bccde98c214499e5a4515a
Shohrux2004 <shohruxergashev67@gmail.com>
2022-10-27 09:42:34 +0500

    3280

--- bin/while_loop09.dart
+++ bin/while_loop09.dart
@@ -2,6 +2,19 @@
   Create function func with "a " argument
   return even numbers as a string
   
-  
 */
-void main() {}
+String func(int a) {
+  int b = 2;
+  String m = '';
+  while (b <= a) {
+    if (b % 2 == 0) {
+      m += b.toString();
+    }
+    b += 1;
+  }
+  return m;
+}
+
+void main() {
+  print(func(20));
+}
