dbd6c392305df4247bbf6d9ab9728597e397f466
Shohrux2004 <shohruxergashev67@gmail.com>
2022-10-31 10:27:27 +0500

    3280

--- bin/while_loop2.dart
+++ bin/while_loop2.dart
@@ -1,11 +1,5 @@
 void main() {
-  Map data = {
-    0: 5,
-    6: 3,
-    7: 6,
-  };
-
-  for (int i in data.values) {
-    print(data);
-  }
+  Map data;
+  data = {1: 'one', 2: 'two'};
+  print(data[1]);
 }
